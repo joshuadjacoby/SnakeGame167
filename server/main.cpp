d1c5dd624c1a251bf3651484b7fba92b3e76ac81
jasaaved <jasaaved@uci.edu>
2016-02-09 11:31:16 -0800

    server code

---
+++ server/main.cpp
@@ -0,0 +1,94 @@
+#include <stdlib.h>
+#include <iostream>
+#include <string>
+#include <sstream>
+#include <time.h>
+#include "websocket.h"
+
+using namespace std;
+
+webSocket server;
+int player_scores[2];
+
+
+
+/* called when a client connects */
+void openHandler(int clientID){
+    ostringstream os;
+    os << "Stranger " << clientID << " has joined.";
+
+    vector<int> clientIDs = server.getClientIDs();
+    for (int i = 0; i < clientIDs.size(); i++){
+        if (clientIDs[i] != clientID)
+            server.wsSend(clientIDs[i], os.str());
+    }
+    server.wsSend(clientID, "Welcome!");
+}
+
+/* called when a client disconnects */
+void closeHandler(int clientID){
+    ostringstream os;
+    os << "Stranger " << clientID << " has leaved.";
+
+    vector<int> clientIDs = server.getClientIDs();
+    for (int i = 0; i < clientIDs.size(); i++){
+        if (clientIDs[i] != clientID)
+            server.wsSend(clientIDs[i], os.str());
+    }
+}
+
+/* called when a client sends a message to the server */
+void messageHandler(int clientID, string message){
+    ostringstream os;
+
+	if (message == "p2score") 
+		player_scores[1]++;
+
+	if (message == "p1score")
+		player_scores[0]++;
+
+	os << "Player 1 score: " << player_scores[0] << "                Player 2 score:" << player_scores[1];
+	cout << os.str();
+
+    vector<int> clientIDs = server.getClientIDs();
+    for (int i = 0; i < clientIDs.size(); i++){
+        if (clientIDs[i] != clientID)
+            server.wsSend(clientIDs[i], os.str());
+    }
+}
+
+/* called once per select() loop */
+void periodicHandler(){
+    static time_t next = time(NULL) + 10;
+    time_t current = time(NULL);
+    if (current >= next){
+        ostringstream os;
+        string timestring = ctime(&current);
+        timestring = timestring.substr(0, timestring.size() - 1);
+        os << timestring;
+
+        vector<int> clientIDs = server.getClientIDs();
+        for (int i = 0; i < clientIDs.size(); i++)
+            server.wsSend(clientIDs[i], os.str());
+
+        next = time(NULL) + 10;
+    }
+}
+
+int main(int argc, char *argv[]){
+    int port;
+
+    cout << "Please set server port: ";
+    cin >> port;
+
+    /* set event handler */
+    server.setOpenHandler(openHandler);
+    server.setCloseHandler(closeHandler);
+    server.setMessageHandler(messageHandler);
+    //server.setPeriodicHandler(periodicHandler);
+
+    /* start the chatroom server, listen to ip '127.0.0.1' and port '8000' */
+    server.startServer(port);
+
+    return 1;
+}
