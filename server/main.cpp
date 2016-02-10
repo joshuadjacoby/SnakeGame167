e7129d1e67a5a87f59c09f4a0fce6e5d7c662b4a
jasaaved <jasaaved@uci.edu>
2016-02-10 13:05:33 -0800

    fixed score bug. added lines to save playerID's.

--- server/main.cpp
+++ server/main.cpp
@@ -9,36 +9,35 @@ using namespace std;
 
 webSocket server;
 int player_scores[2];
+string player1_name;
+string player2_name;
 
 
 
 /* called when a client connects */
-void openHandler(int clientID) {
-	ostringstream os;
-	os << "Stranger " << clientID << " has joined.";
+void openHandler(int clientID){
+	player_scores[0] = 0;
+	player_scores[1] = 0;
+	player1_name = " ";
+	player2_name = " ";
 
-	vector<int> clientIDs = server.getClientIDs();
-	for (int i = 0; i < clientIDs.size(); i++) {
-		if (clientIDs[i] != clientID)
-			server.wsSend(clientIDs[i], os.str());
-	}
 }
 
 /* called when a client disconnects */
-void closeHandler(int clientID) {
-	ostringstream os;
-	os << "Stranger " << clientID << " has leaved.";
+void closeHandler(int clientID){
+    ostringstream os;
+    os << "Stranger " << clientID << " has leaved.";
 
-	vector<int> clientIDs = server.getClientIDs();
-	for (int i = 0; i < clientIDs.size(); i++) {
-		if (clientIDs[i] != clientID)
-			server.wsSend(clientIDs[i], os.str());
-	}
+    vector<int> clientIDs = server.getClientIDs();
+    for (int i = 0; i < clientIDs.size(); i++){
+        if (clientIDs[i] != clientID)
+            server.wsSend(clientIDs[i], os.str());
+    }
 }
 
 /* called when a client sends a message to the server */
-void messageHandler(int clientID, string message) {
-	ostringstream os;
+void messageHandler(int clientID, string message){
+    ostringstream os;
 
 	if (message == "p2score") {
 		player_scores[1]++;
@@ -50,49 +49,55 @@ void messageHandler(int clientID, string message) {
 		os << player_scores[1];
 
 	}
-
-	cout << os.str();
-
-	vector<int> clientIDs = server.getClientIDs();
-	for (int i = 0; i < clientIDs.size(); i++) {
-		if (clientIDs[i] == clientID)
-			server.wsSend(clientIDs[i], os.str());
+	else {
+		if (player1_name == " ")
+			player1_name = message;
+		else {
+			player2_name = message;
+		}
 	}
+	
+
+
+    vector<int> clientIDs = server.getClientIDs();
+    for (int i = 0; i < clientIDs.size(); i++){
+        if (clientIDs[i] == clientID)
+            server.wsSend(clientIDs[i], os.str());
+    }
 }
 
 /* called once per select() loop */
-void periodicHandler() {
-	static time_t next = time(NULL) + 10;
-	time_t current = time(NULL);
-	if (current >= next) {
-		ostringstream os;
-		string timestring = ctime(&current);
-		timestring = timestring.substr(0, timestring.size() - 1);
-		os << timestring;
+void periodicHandler(){
+    static time_t next = time(NULL) + 10;
+    time_t current = time(NULL);
+    if (current >= next){
+        ostringstream os;
+        string timestring = ctime(&current);
+        timestring = timestring.substr(0, timestring.size() - 1);
+        os << timestring;
 
-		vector<int> clientIDs = server.getClientIDs();
-		for (int i = 0; i < clientIDs.size(); i++)
-			server.wsSend(clientIDs[i], os.str());
+        vector<int> clientIDs = server.getClientIDs();
+        for (int i = 0; i < clientIDs.size(); i++)
+            server.wsSend(clientIDs[i], os.str());
 
-		next = time(NULL) + 10;
-	}
+        next = time(NULL) + 10;
+    }
 }
 
-int main(int argc, char *argv[]) {
-	int port;
+int main(int argc, char *argv[]){
+    int port;
 
-	cout << "Please set server port: ";
-	cin >> port;
+    cout << "Please set server port: ";
+    cin >> port;
 
-	/* set event handler */
-	server.setOpenHandler(openHandler);
-	server.setCloseHandler(closeHandler);
-	server.setMessageHandler(messageHandler);
-	//server.setPeriodicHandler(periodicHandler);
+    /* set event handler */
+    server.setOpenHandler(openHandler);
+    server.setCloseHandler(closeHandler);
+    server.setMessageHandler(messageHandler);
+    //server.setPeriodicHandler(periodicHandler);
 
-	/* start the chatroom server, listen to ip '127.0.0.1' and port '8000' */
-	server.startServer(port);
+    /* start the chatroom server, listen to ip '127.0.0.1' and port '8000' */
+    server.startServer(port);
 
-	return 1;
+    return 1;
 }
-
