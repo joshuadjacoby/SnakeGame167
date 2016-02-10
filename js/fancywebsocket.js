5ce35de8f8f897c06c50649f7a82e634abb4c617
jasaaved <jasaaved@uci.edu>
2016-02-10 02:35:47 -0800

    Add change dispatch method

--- js/fancywebsocket.js
+++ js/fancywebsocket.js
@@ -1,44 +1,50 @@
-var FancyWebSocket = function(url)
-{
-	var callbacks = {};
-	var ws_url = url;
-	var conn;
+var FancyWebSocket = function (url) {
+    var callbacks = {};
+    var ws_url = url;
+    var conn;
 
-	this.bind = function(event_name, callback){
-		callbacks[event_name] = callbacks[event_name] || [];
-		callbacks[event_name].push(callback);
-		return this;// chainable
-	};
+    this.bind = function (event_name, callback) {
+        callbacks[event_name] = callbacks[event_name] || [];
+        callbacks[event_name].push(callback);
+        return this;// chainable
+    };
 
-	this.send = function(event_name, event_data){
-		this.conn.send( event_data );
-		return this;
-	};
+    this.send = function (event_name, event_data) {
+        this.conn.send(event_data);
+        return this;
+    };
 
-	this.connect = function() {
-		if ( typeof(MozWebSocket) == 'function' )
-			this.conn = new MozWebSocket(url);
-		else
-			this.conn = new WebSocket(url);
+    this.connect = function () {
+        if (typeof (MozWebSocket) == 'function')
+            this.conn = new MozWebSocket(url);
+        else
+            this.conn = new WebSocket(url);
 
-		// dispatch to the right handlers
-		this.conn.onmessage = function(evt){
-			dispatch('message', evt.data);
-		};
+        // dispatch to the right handlers
+        this.conn.onmessage = function (evt) {
+            dispatch('message', evt.data);
+        };
 
-		this.conn.onclose = function(){dispatch('close',null)}
-		this.conn.onopen = function(){dispatch('open',null)}
-	};
 
-	this.disconnect = function() {
-		this.conn.close();
-	};
+        this.conn.onclose = function () { dispatch('close', null) }
+        this.conn.onopen = function () { dispatch('open', null) }
+    };
 
-	var dispatch = function(event_name, message){
-		var chain = callbacks[event_name];
-		if(typeof chain == 'undefined') return; // no callbacks for this event
-		for(var i = 0; i < chain.length; i++){
-			chain[i]( message )
-		}
-	}
+    this.disconnect = function () {
+        this.conn.close();
+    };
+
+    this.change = function (event_name) {
+        this.conn.onmessage = function (evt) {
+            dispatch(event_name, evt.data);
+        };
+    };
+
+    var dispatch = function (event_name, message) {
+        var chain = callbacks[event_name];
+        if (typeof chain == 'undefined') return; // no callbacks for this event
+        for (var i = 0; i < chain.length; i++) {
+            chain[i](message)
+        }
+    }
 };
