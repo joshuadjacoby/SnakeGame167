c2eb9245c9982f70b58cf4d53eecc1968dabed92
Shawn Ratcliff <shawn.ratcliff@gmail.com>
2016-03-01 18:02:02 -0800

    Fixed MessageDelayer stacked latency bug

--- server/MessageDelayer.cpp
+++ server/MessageDelayer.cpp
@@ -1,35 +1,34 @@
+// Joshua Jacoby
+// Shawn Ratcliff
+// Jonathan Saavedra
+
 #include "MessageDelayer.h"
 
 MessageDelayer::MessageDelayer(int Delay) {
     // Instantiate a binomial distribution with a maximum delay of Delay, and
     // a probability of 0.5 (i.e., average latency at the midpoint of [0, Delay]).
     latencyDistribution = std::binomial_distribution<int>(Delay, 0.5);
-    nextReleaseTime = 0;   // So that we can identify a brand-new MessageDelayer
 }
 
 // Inserts a message into the MessageDelayer
 void MessageDelayer::putMessage(int clientID, std::string message) {
-	std::pair <int, std::string>  message_pair = std::make_pair(clientID, message);
-    messageQueue.push(message_pair);
-    if (nextReleaseTime == 0)
-        updateReleaseTime();
+	queued_msg_t msg(clientID, message, calcReleaseTime());
+	messageQueue.push(msg);
 }
 
 // Fetches the next message from the MessageDelayer, if one is ready
 // (If not, a special error value is sent. )
 std::pair <int, std::string> MessageDelayer::getMessage() {
-    if (isMessageReady() == true && !messageQueue.empty() ) {
-        updateReleaseTime();
-		std::pair <int, std::string> message = messageQueue.front();
+    if (isMessageReady()) {
+		queued_msg_t msg = messageQueue.front();
         messageQueue.pop();
-		return message;
+		return std::pair<int, std::string>(msg.clientID, msg.message);
     } else
         return std::pair <int, std::string>(-1, "");
 }
 
 bool MessageDelayer::isMessageReady() {
-	return (!messageQueue.empty() && (std::chrono::duration_cast<std::chrono::milliseconds>
-		(std::chrono::system_clock::now().time_since_epoch()).count() >= nextReleaseTime));
+	return (!messageQueue.empty() && timeNow() >= messageQueue.front().releaseTime);
 }
 
 // Discard all messages in the buffer
@@ -40,8 +39,12 @@ void MessageDelayer::clear() {
 }
 
 // Calculate and set the next time a message will be ready to be released
-void MessageDelayer::updateReleaseTime(){
+unsigned long long int  MessageDelayer::calcReleaseTime(){
     int delay = latencyDistribution(randomEngine);
-    unsigned long long currentTime = std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::system_clock::now().time_since_epoch()).count();
-    nextReleaseTime = currentTime + delay;
+	return timeNow() + delay;
+}
+
+unsigned long long int MessageDelayer::timeNow() {
+	return std::chrono::system_clock::now().time_since_epoch() /
+		std::chrono::milliseconds(1);
 }
--- server/MessageDelayer.h
+++ server/MessageDelayer.h
@@ -9,21 +9,34 @@
 #include <iostream>
 #include <random>
 
+typedef struct queued_msg_t {
+	int clientID;
+	std::string message;
+	unsigned long long int releaseTime;
+
+	queued_msg_t(int c, std::string m, unsigned long long int r) {
+		clientID = c;
+		message = m;
+		releaseTime = r;
+	}
+};
+
+
 class MessageDelayer{
     
     public:
         void putMessage (int clientID, std::string message); // adds a new message to the queue
-        std::pair <int, std::string> getMessage();                 // gets the next message from queue, if possible
+        std::pair <int, std::string> getMessage();           // gets the next message from queue, if possible
         bool isMessageReady();                          // check whether next message can be retrieved yet
         void clear();                                   // erase everything in the queue
         MessageDelayer(int Delay);
     
     private:
-        void updateReleaseTime();
-        std::queue <std::pair <int, std::string>> messageQueue;  // FIFO queue holds pairs of (int, string)
+        std::queue <queued_msg_t> messageQueue;  // FIFO queue holds pairs of (int, string)
         std::binomial_distribution<int> latencyDistribution;  // A binomial distribution to approximate random latency with an upper limit of t millisenconds and expected latency of (0+t)/2.
         std::default_random_engine randomEngine; // random number engine (required to use binomial_distribution)
-        unsigned long long nextReleaseTime;          // don’t release message until this time (in ms, since epoch)
+		unsigned long long int calcReleaseTime();
+		unsigned long long int timeNow();
 };
 
 #endif
